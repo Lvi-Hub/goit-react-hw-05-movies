5f3cbeeed52374711b6152618392c4c2efde6597
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-06-13 06:50:30 +0200

    movies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,9 +12,9 @@ const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/movies:movieId" element={<MoviesDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
+        <Route path="/movies/:movieId" element={<MoviesDetails />}>
+          <Route path="Cast" element={<Cast />} />
+          <Route path="Reviews" element={<Reviews />} />
         </Route>
       </Route>
     </Routes>
