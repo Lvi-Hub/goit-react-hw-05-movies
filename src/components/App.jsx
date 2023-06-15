56bca6da80960a7b358b76b1662a80c9a75b874a
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-06-15 22:36:05 +0200

    Lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
+import { lazy } from 'react';
 
 // import Home from '../pages/Home/Home';
 // import Movies from '../pages/Movies/Movies';
@@ -8,13 +9,11 @@ import Layout from './Layout/Layout';
 // import Cast from 'components/Cast/Cast';
 // import Reviews from 'components/Reviews/Reviews';
 
-const Home = React.lazy(() => import('pages/Home/Home'));
-const Movies = React.lazy(() => import('pages/Movies/Movies'));
-const MoviesDetails = React.lazy(() =>
-  import('pages/MovieDetails/MovieDetails')
-);
-const Cast = React.lazy(() => import('components/Cast/Cast'));
-const Reviews = React.lazy(() => import('components/Reviews/Reviews'));
+const Home = lazy(() => import('pages/Home/Home'));
+const Movies = lazy(() => import('pages/Movies/Movies'));
+const MoviesDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
 
 const App = () => {
   return (
